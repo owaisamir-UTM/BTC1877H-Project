4fcc5cacfdedf485a139e0121b0cde892f70fbde
owaisamir-UTM <owais.amir@mail.utoronto.ca>
2025-11-27 22:35:00 -0500

    Data is cleaned and ready to model

--- TeamProject.R
+++ TeamProject.R
@@ -8,6 +8,7 @@
 
 library(dplyr)
 library(janitor)
+library(lubridate)
 library(ggplot2)
 library(survival)
 library(glmnet)
@@ -22,34 +23,118 @@ library(gt)
 # Reading in data and copying
 ################################################################################
 
-data <- readxl::read_xlsx("transfusion data.xlsx") |> 
-data <- clean_names(data)
+# Reading data in
+data <- readxl::read_xlsx("transfusion data.xlsx") |> clean_names()
 
+# Defining which subset of columns to keep
 cols_to_keep <- c(
   "study_id_number", "or_date", "death_date", # Identifiers
   "age", "height", "weight", "gender_male",   # Demographics
-  "type", "redo_lung_transplant", "preoperative_ecls", "ecls_cpb", # Risk Factors
+  "type", "redo_lung_transplant", "intraoperative_ecls", "ecls_cpb", # Risk Factors
   "cystic_fibrosis", "idiopathic_pulmonary_hypertension",  # Diagnosis
   "las_score", "pre_hb", "pre_platelets", "pre_inr",       # Labs
-  "total_24hr_rbc", "duration_of_icu_stay_days", "alive_12mths_yn" # Outcomes
+  "total_24hr_rbc", "duration_of_icu_stay_days", "alive_12mths_yn", "massive_transfusion" # Outcomes
 )
 
+# Copying data to a new clean dataframe
 clean_df <- data[ ,cols_to_keep]
+
+# Cleaning up environment
+rm(cols_to_keep)
 
 ################################################################################
 # Cleaning up data types and setting factors
 ################################################################################
 
+# Looking at the data structures
+str(clean_df)
+
+# Summary statistics of the data frame
+summary(clean_df)
+
+# Looking for any columns that have NA values in them
+na_check <- apply(clean_df, 2, function(x) any(is.na(x)))
+
+# Cleaning up data types
+clean_df$death_date <- as.POSIXct(
+  strptime(clean_df$death_date, format = "%d-%b-%Y"),
+  tz = "UTC"
+)
+
+cols_to_clean <- c("gender_male", "redo_lung_transplant", "ecls_cpb", "cystic_fibrosis", "idiopathic_pulmonary_hypertension")
+clean_df <- clean_df |> 
+  mutate(across(all_of(cols_to_clean), ~ if_else(. == "FALSE", FALSE, TRUE)))
+
+clean_df <- clean_df |> rename("gender" = "gender_male")
+clean_df$gender <- factor(clean_df$gender,
+                          levels = c(FALSE, TRUE),
+                          labels = c("Female", "Male"))
+
+clean_df$massive_transfusion <- if_else(clean_df$massive_transfusion == 1, TRUE, FALSE)
+
+# Collapsing "Single Left Lung" and "Single Right Lung" as just "Single"
+clean_df$type <- if_else(clean_df$type == "Bilateral", "Bilateral", "Single")
+clean_df$type <- factor(clean_df$type, levels = c("Single", "Bilateral"))
+
+clean_df$alive_12mths_yn <- if_else(clean_df$alive_12mths_yn == "Y", TRUE, FALSE)
+clean_df <- clean_df |> rename("alive_12mths" = "alive_12mths_yn")
+
+summary(clean_df)
+
+# Cleaning up environment
+rm(cols_to_clean, na_check)
 
 ################################################################################
 # Creating composite variables
 ################################################################################
 
+# Event variable where event (1= died, 0=alive/censored)
+clean_df["event"] <- if_else(clean_df$alive_12mths == "TRUE", 0, 1)
+
+# Calculating days alive post OR date up until censoring
+clean_df["survival_time_raw"] <- as.numeric(clean_df$death_date - clean_df$or_date)
+clean_df["survival_time"] <- if_else(clean_df$event == 1 & clean_df$survival_time_raw <= 365,
+                                     clean_df$survival_time_raw, 365)
+
+# Calculating BMI from weight and height
+clean_df["bmi"] <- clean_df$weight/((clean_df$height/100)**2)
+
+# Classifying a high risk patient 
+clean_df["high_risk_patient"] <- if_else(clean_df$redo_lung_transplant == TRUE |
+                                           clean_df$intraoperative_ecls == TRUE |
+                                           clean_df$ecls_cpb == TRUE |
+                                           clean_df$cystic_fibrosis == TRUE |
+                                           clean_df$idiopathic_pulmonary_hypertension == TRUE, TRUE, FALSE)
+
+# Classifying patients more likely to bleed due to coagulopathy
+clean_df["baseline_coagulopathy"] <- if_else(clean_df$pre_inr > 1.5 | clean_df$pre_platelets < 150, TRUE, FALSE)
 
 ################################################################################
 # Table 1 summary statistics
 ################################################################################
 
+
 ################################################################################
-# 1.1 
+# Sub-Analysis 1.1: The "Screening" Model (Who needs ANY blood?)
 ################################################################################
+
+
+################################################################################
+# Sub-Analysis 1.2: The "Crisis" Model (Who needs MASSIVE blood?)
+################################################################################
+
+
+################################################################################
+# Sub-Analysis 1.3: The "Volume" Model (What drives amount?)
+################################################################################
+
+
+################################################################################
+# Sub-Analysis 2.1: Survival Analysis (Mortality)
+################################################################################
+
+
+################################################################################
+# Sub-Analysis 2.2: Resource Utilization (ICU LOS) 
+################################################################################
+
